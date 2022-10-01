46a0153a52b07d8a0fdf8749fa27d08e2245009f
Mikael Gueck <gumi@iki.fi>
2022-10-01 16:39:36 +0300

    dox

--- poc_statemachine_1.py
+++ poc_statemachine_1.py
@@ -5,40 +5,45 @@ _FILENAME = '../gpcaxis/data/010_kats_tau_101.px'
 
 
 class CounterParser:
+    """A POC for doing preliminary non-validating parsing for the
+    header section of a PX file with counters and one accumulator
+    only, rather than a more complex state machine.
+    """
 
+    # How many of these characters we've seen in this parse
     _quotes, _semicolons, _equals, _commas = 0, 0, 0, 0
 
-    # sb = square brackets, p = parenthesis
+    # sb = square brackets open close, p = parenthesis
     _sbo, _sbc, _po, _pc = 0, 0, 0, 0
 
     count, s, keys, values = 0, "", list(), list()
 
     def _niq(self) -> bool:
-        """Not in quotes"""
+        """The character pointer/cursor is currently not in a
+        location in the PX file that's inside a quoted string."""
         return self._quotes % 2 == 0
-
-    def __str__(self) -> str:
-        return 'sbo: {}, sbc: {}, po: {}, pc: {}, quotes: {}, semis: {}, equals: {}'.format(
-            self._sbo, self._sbc, self._po, self._pc,
-            self._quotes, self._semicolons, self._equals
-        )
 
     def parse(self, c: str) -> bool:
         self.count += 1
         if c == '"':
             self._quotes += 1
         elif c == '\n' or c == '\r':
-            # no newlines inside quoted strings
+            # There can't be newlines inside quoted strings.
             return True
         elif c == '(' and self._niq() and self._po == self._pc:
+            # There can't be nested parentheses, only one can be open.
             self._po += 1
         elif c == ')' and self._niq() and self._po > self._pc:
+            # There can't be a close parentheses if none is open.
             self._pc += 1
         elif c == '[' and self._niq() and self._sbo == self._sbc:
+            # There can't be nested square brackets, only one can be open.
             self._sbo += 1
         elif c == ']' and self._niq() and self._sbo > self._sbc:
+            # There can't be a close square brackets if none is open.
             self._sbc += 1
-        elif c == '=' and self._niq():
+        elif c == '=' and self._niq() and \
+                self._semicolons == self._equals:
             # end of key
             if self.s == 'DATA':
                 return False
@@ -46,7 +51,8 @@ class CounterParser:
             self.keys.append(self.s)
             self.s = ''
             return True
-        elif c == ';' and self._niq():
+        elif c == ';' and self._niq() and \
+                self._semicolons < self._equals:
             # end of value
             self._semicolons += 1
             self.values.append(self.s)
@@ -54,6 +60,12 @@ class CounterParser:
             return True
         self.s += c
         return True
+
+    def __str__(self) -> str:
+        return 'sbo: {}, sbc: {}, po: {}, pc: {}, quotes: {}, semis: {}, equals: {}'.format(
+            self._sbo, self._sbc, self._po, self._pc,
+            self._quotes, self._semicolons, self._equals
+        )
 
 
 def main():
