c36fc1d1e172636245b00117cd271e35fe81a1cd
Mikael Gueck <gumi@iki.fi>
2022-10-03 16:37:20 +0300

    Start breaking out state.

--- poc_statemachine_2.py
+++ poc_statemachine_2.py
@@ -4,6 +4,8 @@ import argparse
 from dataclasses import dataclass, field
 import io
 
+from typing.io import TextIO
+
 
 class ParseException(Exception):
     """PX parse failed"""
@@ -14,6 +16,7 @@ class PxHeaderKeyword:
     keyword: str = ''
     language: str = ''
     subkeys: list[str] = field(default_factory=list)
+
     def __hash__(self) -> int:
         if self.subkeys:
             return hash((self.keyword, self.language, *self.subkeys))
@@ -24,6 +27,7 @@ class PxHeaderKeyword:
 @dataclass
 class PxHeaderValue:
     values: list[str] = field(default_factory=list)
+
     def __hash__(self) -> int:
         return hash(tuple(self.values))
 
@@ -43,15 +47,29 @@ class RowAccumulator:
     value: str = ''
     values: list[str] = field(default_factory=list)
 
-    def get_keyword(self) -> PxHeaderKeyword:
+    def to_keyword(self) -> PxHeaderKeyword:
         return PxHeaderKeyword(
             self.keyword,
             self.language if self.language != '' else None,
             self.subkeys if len(self.subkeys) > 0 else None
         )
 
-    def get_value(self) -> PxHeaderValue:
+    def to_value(self) -> PxHeaderValue:
         return PxHeaderValue(self.values)
+
+    def to_row(self) -> PxHeaderRow:
+        return PxHeaderRow(self.to_keyword(), self.to_value())
+
+
+@dataclass
+class HeaderParseState:
+    quotes: int = 0
+    semicolons: int = 0
+    equals: int = 0
+    squarebracket_open: int = 0
+    squarebracket_close: int = 0
+    parenthesis_open: int = 0
+    parenthesis_close: int = 0
 
 
 class CounterParser(object):
@@ -61,71 +79,61 @@ class CounterParser(object):
     """
 
     chunk_size = 4096
-
-    # How many of these characters we've seen in this parse
-    _quotes: int = 0
-    _semicolons: int = 0
-    _equals: int = 0
-    _squarebracket_open: int = 0
-    _squarebracket_close: int = 0
-    _parenthesis_open: int = 0
-    _parenthesis_close: int = 0
-
     count: int = 0
     headers = dict()
     row = RowAccumulator()
+    hs = HeaderParseState()
 
-
-    def parse_file(self, f: io.FileIO):
+    def parse_file(self, f: TextIO):
         while data := f.read(self.chunk_size):
             for c in data:
                 if self.parse_character(c):
                     return
-
 
     def parse_character(self, c: str) -> bool:
         """
         Returns a bool to signify whether we should stop parsing here.
         """
 
-        in_quotes = self._quotes % 2 == 1
-        in_parenthesis = self._parenthesis_open > self._parenthesis_close
-        in_key = self._semicolons == self._equals
-        in_language = in_key and self._squarebracket_open > self._squarebracket_close
+        in_quotes = self.hs.quotes % 2 == 1
+        in_parenthesis = self.hs.parenthesis_open > self.hs.parenthesis_close
+        in_key = self.hs.semicolons == self.hs.equals
+        in_language = in_key and self.hs.squarebracket_open > self.hs.squarebracket_close
         in_subkey = in_key and in_parenthesis
 
         self.count += 1
 
         if c == '"':
-            self._quotes += 1
+            self.hs.quotes += 1
 
-        elif c == '\n' or c == '\r':
-            if in_quotes:
-                raise ParseException("There can't be newlines inside quoted strings.")
+        elif (c == '\n' or c == '\r') and in_quotes:
+            raise ParseException("There can't be newlines inside quoted strings.")
+
+        elif (c == '\n' or c == '\r') and not in_quotes:
             return False
 
         elif c == '[' and in_key and not in_quotes:
-            self._squarebracket_open += 1
+            self.hs.squarebracket_open += 1
 
         elif c == ']' and in_key and not in_quotes:
-            self._squarebracket_close += 1
+            self.hs.squarebracket_close += 1
 
         elif c == '(' and in_key and not in_quotes:
-            self._parenthesis_open += 1
+            self.hs.parenthesis_open += 1
 
+        # TLIST opening quote
         elif c == '(' and not in_key and not in_quotes:
-            # TLIST(
-            self._parenthesis_open += 1
+            self.hs.parenthesis_open += 1
             self.row.value += c
 
         elif c == ')' and in_key and not in_quotes:
-            self._parenthesis_close += 1
+            self.hs.parenthesis_close += 1
             self.row.subkeys.append(self.row.subkey)
             self.row.subkey = ''
 
+        # TLIST closing quote
         elif c == ')' and not in_key and not in_quotes:
-            # TLIST()
-            self._parenthesis_close += 1
+            self.hs.parenthesis_close += 1
             self.row.value += c
 
         elif c == ',' and in_subkey and not in_quotes:
@@ -143,17 +151,18 @@ class CounterParser(object):
         elif c == '=' and in_key and not in_quotes:
             if self.row.keyword == 'DATA':
                 return True
-            self._equals += 1
+            self.hs.equals += 1
             return False
 
-        elif c == ';' and not in_quotes:
-            if in_key:
-                raise ParseException(
-                    "Found a semicolon without a matching equals sign. Value terminator without keyword terminator.")
+        elif c == ';' and not in_quotes and in_key:
+            raise ParseException(
+                "Found a semicolon without a matching equals sign. Value terminator without keyword terminator.")
+
+        elif c == ';' and not in_quotes and not in_key:
             if len(self.row.value) > 0:
                 self.row.values.append(self.row.value)
-            self._semicolons += 1
-            self.headers[self.row.get_keyword()] = self.row.get_value()
+            self.hs.semicolons += 1
+            self.headers[self.row.to_keyword()] = self.row.to_value()
             self.row = RowAccumulator()
             return False
 
@@ -171,15 +180,11 @@ class CounterParser(object):
 
         return False
 
-
     def values(self, keyword: str, language: str = None, subkeys: list[str] = None):
         return self.headers[PxHeaderKeyword(keyword, language, subkeys)].values
 
-
     def __str__(self) -> str:
-        return 'count: {}, quotes: {}, semis: {}, equals: {}'.format(
-            self.count, self._quotes, self._semicolons, self._equals
-        )
+        return f'{self.hs=}'
 
 
 def _parse_args():
